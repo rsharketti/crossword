cfbe42098148c27dd4c58e5332141976e8b1ff2c
rsharketti <rsharketti@gmail.com>
2015-02-12 12:05:28 -0500

    First Benchmark.
    Placing text in the document and starting to place things where I would
    like them with CSS. Also, start of finding a solution to create a word
    search.

---
+++ js/script.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+
+onload = init;
+
+function init(){
+	document.getElementById('1-1').onmousedown = function(){
+		scrollover('2-2');
+	}
+};
+
+
+
+function scrollover(idnum){
+	document.getElementById(idnum).style.backgroundColor = "#f0c";
+};
+
+function userClick(){}
+
+
+function makeSelection(){
+		
+}
+
+
+
+
+
